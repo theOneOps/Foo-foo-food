0c187563eeb937828e4c8db814b84008e63d8ad6
Aymeric <roua4409@usherbrooke.ca>
2024-09-19 14:14:13 -0400

    ressources

--- app/src/main/java/com/uds/foufoufood/activities/login/LoginActivity.kt
+++ app/src/main/java/com/uds/foufoufood/activities/login/LoginActivity.kt
@@ -1,12 +1,6 @@
 package com.uds.foufoufood.activities.login
 
-import android.app.Dialog
 import android.os.Bundle
-import android.util.TypedValue
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -18,7 +12,7 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
+        setContentView(R.layout.layout_register)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/uds/foufoufood/activities/main/MainActivity.kt
+++ app/src/main/java/com/uds/foufoufood/activities/main/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.uds.foufoufood.R
-import android.widget.Button
 import android.widget.TextView
 import com.uds.foufoufood.activities.login.LoginActivity
 
@@ -16,7 +15,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.layout_welcome)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
